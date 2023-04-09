755eee6ad175473fa03ef58bebfc959d5f2bc829
armakgul <armakgul@gmail.com>
2023-04-09 11:24:22 +0300

    last

--- Assets/Assets/Scripts/CollectAndDie.cs
+++ Assets/Assets/Scripts/CollectAndDie.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectAndDie : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text scoreText;
+
+    private void OnCollisionEnter2D(Collision2D other) 
     {
         
+        if(other.gameObject.tag == "hearth")
+        {
+            Debug.Log("hearth");
+        }
+    
     }
 
     // Update is called once per frame
