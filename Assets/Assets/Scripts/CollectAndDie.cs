0912029efbed80ade8f2899c57341825404f71f9
armakgul <armakgul@gmail.com>
2023-04-09 12:15:27 +0300

    aa

--- Assets/Assets/Scripts/CollectAndDie.cs
+++ Assets/Assets/Scripts/CollectAndDie.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CollectAndDie : MonoBehaviour
 {
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
+    float score = 1;
 
-    private void OnCollisionEnter2D(Collision2D other) 
-    {
+    private void OnTriggerEnter2D(Collider2D other) {
         
-        if(other.gameObject.tag == "hearth")
+
+        if (other.gameObject.tag == "hearth")
         {
-            Debug.Log("hearth");
+            Debug.Log(other.gameObject.tag);
+            scoreText.text = score.ToString();
+            score++;
         }
-    
+
+        if (other.gameObject.tag == "obstacle")
+        {
+            Debug.Log("dead");
+        }
     }
 
     // Update is called once per frame
